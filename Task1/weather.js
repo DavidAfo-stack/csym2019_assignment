46d4b28724f0dd56891a9122646e935fbeb7aa61
DavidAfo-Stack <afolabidavid424@gmail.com>
2021-05-10 18:27:40 +0100

    styling added for Task 1

--- Task1/weather.js
+++ Task1/weather.js
@@ -13,7 +13,7 @@ window.onload = function() {
             }, 5000);
         })
         .catch((error) => {
-            var div = document.getElementById('update-message');
+            var div = document.getElementById('update-status-message');
             div.append('Error Loading Data');
             console.log('Error loading data: ' + error);
         });
@@ -33,8 +33,35 @@ window.onload = function() {
 
         table.append(row)
 
+        const generateIcons = (description) => {
+            if (description == 'snow') {
+                return "../weather_icons/snow.png";
+            } else if (description == 'cloud') {
+                return "../weather_icons/cloud.png";
+            } else if (description == 'sleet') {
+                return "../weather_icons/sleet.png";
+            } else if (description == 'sun') {
+                return "../weather_icons/sun.png";
+            } else if (description == 'hail') {
+                return "../weather_icons/hail.png";
+            } else if (description == 'heavy cloud') {
+                return "../weather_icons/heavycloud.png";
+            } else if (description == 'heavy rain') {
+                return "../weather_icons/heavyrain.png";
+            } else if (description == 'mist') {
+                return "../weather_icons/mist.png";
+            } else if (description == 'rain') {
+                return "../weather_icons/rain.png";
+            } else if (description == 'sun and cloud') {
+                return "../weather_icons/sun_and_cloud.png";
+            } else if (description == 'thunderstorm') {
+                return "../weather_icons/thunderstorm.png";
+            }
+        }
+
         for (let i = 0; i < data.cities.length; i++) {
             let row = document.createElement('tr');
+            row.setAttribute('class', "active-row")
             row.innerHTML =
                 '<td>' +
                 data.cities[i].id +
@@ -47,7 +74,7 @@ window.onload = function() {
                 '</td>' +
                 `<td><img src = ${generateIcons(data.cities[i].currentCondition)}  alt = ""  width= 35px height = 35px></td>` +
                 '<td>' +
-                data.cities[i].temperature +
+                data.cities[i].temperature + "°C" +
                 '</td>' +
                 '<td>' +
                 data.cities[i].windSpeed +
@@ -63,30 +90,4 @@ window.onload = function() {
         }
 
     };
-
-    const generateIcons = (description) => {
-        if (description == 'snow') {
-            return "../weather_icons/snow.png";
-        } else if (description == 'cloud') {
-            return "../weather_icons/cloud.png";
-        } else if (description == 'sleet') {
-            return "../weather_icons/sleet.png";
-        } else if (description == 'sun') {
-            return "../weather_icons/sun.png";
-        } else if (description == 'hail') {
-            return "../weather_icons/hail.png";
-        } else if (description == 'heavy cloud') {
-            return "../weather_icons/heavycloud.png";
-        } else if (description == 'heavy rain') {
-            return "../weather_icons/heavyrain.png";
-        } else if (description == 'mist') {
-            return "../weather_icons/mist.png";
-        } else if (description == 'rain') {
-            return "../weather_icons/rain.png";
-        } else if (description == 'sun and cloud') {
-            return "../weather_icons/sun_and_cloud.png";
-        } else if (description == 'thunderstorm') {
-            return "../weather_icons/thunderstorm.png";
-        }
-    }
 };
